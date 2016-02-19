7723abb0b58be4a077f42f1a43efbe492c872ec3
kissa1001 <kissa101295@gmail.com>
2016-02-19 15:50:53 -0500

    fix pipe

--- js/flappy_bird.js
+++ js/flappy_bird.js
@@ -1,27 +1,13 @@
 var graphicsSystem = require('./systems/graphics');
-var pipeSystem = require('./systems/pipe-graphics');
 var bird = require('./entities/bird');
 var pipe = require('./entities/pipe');
 var FlappyBird = function() {
-    this.entities = [new bird.Bird()];
+    this.entities = [new bird.Bird(), new pipe.Pipe()];
     this.graphics = new graphicsSystem.GraphicsSystem(this.entities);
-   // this.pipe = new pipeSystem.pipeSystem(this.entities);
 };
 
 FlappyBird.prototype.run = function() {
     this.graphics.run();
-    this.pipe.run();
 };
 
 exports.FlappyBird = FlappyBird;
-
-var Pipes = function() {
-    this.entities = [new pipe.Pipe()];
-    this.graphics = new graphicsSystem.GraphicsSystem(this.entities);
-};
-
-Pipes.prototype.run = function() {
-    this.graphics.run();
-};
-
-exports.Pipes = Pipes;
