636a4f5380f6c61f06d5913329b30917248de311
kissa1001 <kissa101295@gmail.com>
2016-02-19 16:37:15 -0500

    bird added

--- js/components/graphics/bird.js
+++ js/components/graphics/bird.js
@@ -1,9 +1,10 @@
 var BirdGraphicsComponent = function(entity) {
     this.entity = entity;
+    this.image = document.getElementById("mario");
 };
 
-BirdGraphicsComponent.prototype.draw = function() {
-    console.log("Drawing a bird");
+BirdGraphicsComponent.prototype.draw = function(context) {
+    context.drawImage(this.image, 0, 0);
 };
 
 exports.BirdGraphicsComponent = BirdGraphicsComponent;
